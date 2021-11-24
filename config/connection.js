8acc4d5758fb1ee3e99aeeb28c5559815aa39503
Raimeeab <raimee.abbassi@gmail.com>
2021-11-24 19:42:21 +1100

    Set up server, config & schema

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+// const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up Handlebars.js engine with custom helpers
+// const hbs = exphbs.create({ helpers });
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  }); 
