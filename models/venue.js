abd985f3b8ac44be2cdc57f302507cf35d9273fe
unknown <aszonyi49@gmail.com>
2021-11-25 20:56:26 +1100

    added Venue table and updated index.js

---
+++ models/venue.js
@@ -0,0 +1,54 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our Bandmodel
+class Venue extends Model {}
+
+// create fields/columns for Band model
+Venue.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+    phoneNumber: {
+      type: DataTypes.INTEGER,
+      allowNull:false,
+    },
+    capacity: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'venue'
+  }
+);
+
+module.exports = Venue;
