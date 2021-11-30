ed3c695a47d0262a70de84aad619e066323a9bd2
Raimeeab <raimee.abbassi@gmail.com>
2021-11-30 14:26:39 +1100

    Update handlebars, adjust vendor/ venue routes

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,27 +1,54 @@
+const eventName = document.querySelector('#event-name').value.trim();
+const eventType = document.querySelector('#event-type');
+const eventBuget = document.querySelector('#event-budget').value.trim();
+const attendees = document.querySelector('#event-guest').value.trim();
+
+// Used to dictate if event needs venue
+const venueYes = document.querySelector('#venue-yes');
+const venueNo = document.querySelector('#venue-no');
+
+// Used to display venue details (if selected)
+// const eventVenue = 
+
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#event-name').value.trim();
-  const event_buget = document.querySelector('#event-budget').value.trim();
-  const attendees = document.querySelector('#event-guest').value.trim();
-  // const venueQuery = document.querySelector('#event-venue'); 
 
-  if (name && event_buget && attendees) {
-    const response = await fetch(`/api/events`, {
-      method: 'POST',
-      body: JSON.stringify({ name, event_buget, attendees }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+  // if (name && event_buget && attendees) {
+  //   const response = await fetch(`/api/events`, {
+  //     method: 'POST',
+  //     body: JSON.stringify({ name, event_buget, attendees }),
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //   });
 
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert('Failed to create event');
-    };
-  };
+  //   if (response.ok) {
+  //     document.location.replace('/profile');
+  //   } else {
+  //     alert('Failed to create event');
+  //   };
+  // };
 };
+
+const viewEvent = async (event) => {
+  const response = await fetch(`/api/events/:id`, {
+    method: 'GET',
+    body: JSON.stringify({ eventName, eventType, eventBuget, attendees, eventVenue }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace('/profile/events/:id');
+  } else {
+    alert('Failed to view event');
+  };
+
+};
+
+
 
 const delButtonHandler = async (event) => {
   if (event.target.hasAttribute('data-id')) {
