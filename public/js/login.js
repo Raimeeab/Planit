ad0ab30e2b1f7d6499ac0af08a39f80fa34a8d8f
nicholas <n1ckorish14@gmail.com>
2021-11-30 20:09:51 +1100

    updated the show login and hide register details on login.handlebar, added function to login.js

--- public/js/login.js
+++ public/js/login.js
@@ -54,7 +54,14 @@ const loginFormHandler = async (event) => {
     .querySelector('.signup-form')
     .addEventListener('submit', signupFormHandler);
 
-  // LOGIN/REGISTER TOGGLE
+
+  // LOGIN/REGISTER TOGGLE ----------------------------------------------------------------------
   const toggle = document.querySelector('#register-login-toggle');
-  const content = document.querySelector('.toggle-content')
   
+  register-login-toggle.addEventListener('click', () =>{
+    console.log('clicked!')
+    const content = document.querySelector('.toggle-content')
+    const ariaHidden = content.getAttribute('aria-hidden')
+
+    content.setAttribute('aria-hidden', ariaHidden === 'true' ? 'false' : 'true') 
+  });  
