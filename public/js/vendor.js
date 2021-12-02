1e600723ce1d64047d40dbc39ec6a214b178c748
Raimeeab <raimee.abbassi@gmail.com>
2021-12-02 20:50:37 +1100

    Debug view by event id

--- public/js/vendor.js
+++ public/js/vendor.js
@@ -35,10 +35,3 @@ const loadVendorById = async (vendor) => {
     })
     
 }
-// document
-//     .querySelector('#nav-vendors')
-//     .addEventListener('click', loadVendors);
-
-// document
-//     .querySelector('.vendor-id')
-//     .addEventListener('click', loadVendorById);
