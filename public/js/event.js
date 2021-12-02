8b2dc8685f2996af1af66454d5cbf7433402ff29
Raimeeab <raimee.abbassi@gmail.com>
2021-12-02 20:20:08 +1100

    Adding events handlebars

---
+++ public/js/event.js
@@ -0,0 +1,8 @@
+const loadEvent = async (event) => {
+    // {"id":2,"name":"Koren's Hens Party","type":"Hens","budget":"1000.00","attendees":30,"date":"4/12/2021","venue_id":null,"user_id":1}
+    const eventName = document.querySelector('.event-name');
+    const eventType = document.querySelector('.event-type');
+    const eventBudget = document.querySelector('.event-budget');
+    const eventGuests = document.querySelector('.event-guest');
+    
+}
